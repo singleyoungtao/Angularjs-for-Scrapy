ff9be90e53c9353f736ed9a6d7a99da23cccb71c
singleyoungtao <singleyoungtao@gmail.com>
2017-04-24 16:06:25 +0800

    接受json的服务测试初步代码完成，下一步是跨域

--- app/app.js
+++ app/app.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('app', [
-    'ngRoute'
+    'ngRoute',
+    'ngResource'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
+    $routeProvider.when('/', {
+        templateUrl: 'view/test.html',
+        controller: 'showPageCtrl'
+    });
     $routeProvider.otherwise({ redirectTo: '/' });
-}]);
+}]).
+factory('GetResults', ['$resource', function($resource) {
+        return $resource('http://127.0.0.1:5000/results', null, {
+            action: {
+                method: 'GET',
+                url: 'someOtherUrl',
+                isArray: false
+            }
+        });
+    }])
+    .controller('showPageCtrl', ['$scope', 'GetResults', function($scope, GetResults) {
+        var pageshow = GetResults.query()
+        $scope.results = pageshow['results']
+    }])
