ac4fa182cb639d03cf64cd05a77af904c7850449
singleyoungtao <singleyoungtao@gmail.com>
2017-04-27 00:11:07 +0800

    ，

--- app/app.js
+++ app/app.js
@@ -14,42 +14,104 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
         });
         $routeProvider.otherwise({ redirectTo: '/' });
     }])
-    .factory('GetResults', ['$resource', function($resource) {
-        return $resource('http://127.0.0.1:5000/results', null, {
-            action: {
-                method: ['GET', 'POST'],
-                url: 'someOtherUrl',
-                isArray: false
-            }
-        });
-    }])
-    // .factory('PostKeywords', ['$resource', function($resource) {
-    //     return $resource('http://127.0.0.1:5000/keywords', null, {
-    //         action: {
-    //             method: 'POST',
-    //             url: 'someOtherUrl',
+    // .factory('GetUrl', ['$resource', function($resource) {
+    //     return $resource('http://127.0.0.1:5000/url-get', null, {
+    //         get: {
+    //             method: 'GET',
+    //             url: 'http://127.0.0.1:5000/url-get',
     //             isArray: false
     //         }
     //     });
     // }])
-    .controller('showPageCtrl', ['$scope', 'GetResults',
-        function($scope, GetResults) {
-            $scope.searched = false
+    // .factory('PostUrl', ['$http', function($http){
+    //     return http://127.0.0.1:5000/url-post
+    // }])
+    // .factory('PostUrl', ['$resource', function($resource) {
+    //     return $resource('http://127.0.0.1:5000/url-post', null, {
+    //         save: {
+    //             method: ['GET', 'POST'],
+    //             url: 'http://127.0.0.1:5000/url-post',
+    //             isArray: false
+    //         }
+    //     });
+    // }])
+    // .factory('GetResults', ['$resource', function($resource) {
+    //     return $resource('http://127.0.0.1:5000/results', null, {
+    //         save: {
+    //             method: ['GET', 'POST'],
+    //             url: 'http://127.0.0.1:5000/results',
+    //             isArray: false
+    //         }
+    //     });
+    // }])
+    .controller('showPageCtrl', ['$scope', '$http',
+        function($scope, $http) {
+            // $scope.defaulturl = GetUrl.get();
+            $http.get('http://127.0.0.1:5000/url-get').then(function(resp) {
+                console.log("123")
+                console.log(resp);
+                $scope.defaulturl = resp.data;
+                console.log($scope.defaulturl.posturl);
+
+            }, function(error) {
+                alert("Error!")
+            });
+            // console.log($scope.defaultrul.posturl);
+            $scope.checkpasswordview = false;
+            $scope.checkpass = false;
+            $scope.searched = false;
+            $scope.changeUrl = function() {
+                $scope.checkpasswordview = true;
+            };
+            $scope.checkPassword = function() {
+                if ($scope.inputpassword == "nwsuaf") {
+                    $scope.checkpass = true;
+                    $scope.checkpasswordview = false;
+                } else {
+                    console.log("密码错误");
+                    alert("密码错误！");
+                }
+            }
+            $scope.sendUrlClick = function() {
+                $http.post('http://127.0.0.1:5000/url-post', { posturl: $scope.inputurl }).then(function(resp) {
+                        console.log("发送成功");
+                        // $scope.defaulturl = GetUrl.get();
+                        // $scope.defaulturl = $http.get('http://127.0.0.1:5000/url-get').data
+                        $scope.defaulturl.posturl = resp.data
+                        console.log($scope.defaulturl.posturl);
+                    }, function(error) {
+                        alert("发送请求失败！")
+                        console.log(error.status);
+                    })
+                    // PostUrl.save({}, { posturl: $scope.inputurl }, function(resp) {
+                    //     console.log("发送成功");
+                    //     $scope.defaulturl = GetUrl.get();
+                    // }, function(error) {
+                    //     alert("发送请求失败！")
+                    //     console.log(error.status);
+                    // })
+
+            }
             $scope.getResultsClick = function() {
-                GetResults.save({}, { keywords: $scope.inputkeywords }, function(resp) {
+                // GetResults.save({}, { keywords: $scope.inputkeywords }, function(resp) {
+                $http.post('http://127.0.0.1:5000/results', { keywords: $scope.inputkeywords }).then(function(resp) {
                     // var pageshow = GetResults.get()
                     // 'get': {method:'GET'},  'query':  {method:'GET', isArray:true}
-                    var pageshow = resp
-                        // post方法直接就有返回的response，不用再次发get请求
-                    $scope.pageshowcopy = pageshow
-                    $scope.pageshowresults = $scope.pageshowcopy.results
-                    $scope.teststr = "pageshow"
-                        // $scope.c = GetResults.get()
-                        // 得到传过来的json后可以直接使用。
-                    $scope.searched = true
+                    var pageshow = resp.data;
+                    // post方法直接就有返回的response，不用再次发get请求
+                    $scope.pageshowcopy = pageshow;
+                    $scope.pageshowresults = $scope.pageshowcopy.results;
+                    $scope.teststr = "pageshow";
+                    // $scope.c = GetResults.get()
+                    // 得到传过来的json后可以直接使用。
+                    $scope.searched = true;
                 }, function(error) {
-                    console.log(error.status)
-                })
+                    $scope.searched = false;
+                    alert("发送请求失败！");
+                    console.log(error.status);
+                });
+
+
             }
 
         }
